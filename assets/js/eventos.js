7c572c836167d18504599d30ed4a53c9a8337fac
= <DOT175@PCDiseno5.rednet1.lan>
2015-11-26 10:26:06 -0300

    agrego assets

---
+++ assets/js/eventos.js
@@ -0,0 +1,144 @@
+
+$(document).ready(function(){
+
+	$(window).resize(function() {
+		var windowHeight = $(window).height();
+		if(windowHeight > 1000){
+			$('.footer').css('position', 'fixed');
+			$('.contenedor-encuesta').css('padding-bottom', '130px');
+		}else{
+			$('.footer').css('position', 'relative');
+			$('.contenedor-encuesta').css('padding-bottom', '130px');
+		}
+	});
+
+	var windowHeight = $(window).height();
+	console.log(windowHeight);
+	if(windowHeight > 1000){
+		$('.footer').css('position', 'fixed');
+	}else{
+		$('.footer').css('position', 'relative');
+	}
+
+
+
+	/**
+	 * Busqueda de productos en la home
+	 * Facil...
+	 */
+
+	 $('.find-input-in-results').keyup(function(event) {
+	 	var collection = $('.find-box-in-results');
+	 	var find = $(this).val().toUpperCase();
+	 	if (find != "") {
+		 	$('.find-box-in-results').parent().parent().hide();
+		 	$.each(collection, function(index, val) {
+		 		var title = $(val).text().toUpperCase();
+		 		if (title.search(find) != -1) {
+		 			$(val).parent().parent().show();
+		 		};
+		 	});
+	 	}else{
+	 		$('.find-box-in-results').parent().parent().show();
+	 	}
+	 });
+
+
+	 $(window).bind('loadAjax');
+	 /**
+	  * Combo en historial.php
+	  */
+	 
+	 $('#findBy').change(function(event) {
+	 	event.preventDefault();
+	 	var obj = $(this);
+	 	var comboSelect = $('#results');
+
+
+		/**
+		 * cambio el nombre del select segun la opcion
+		 */
+
+	 	switch(obj.val()){
+	 		case '1':
+	 		comboSelect.attr('name', 'producto');
+	 		break;
+	 		case '2':
+	 		comboSelect.attr('name', 'estado');
+	 		break;
+	 		case '3':
+	 		comboSelect.attr('name', 'remito');
+	 		break;
+	 		case '4':
+	 		comboSelect.attr('name', '');
+	 		break;
+
+	 		default:
+	 		comboSelect.attr('name', '');
+
+	 		break;
+	 	}
+
+	 	if (obj.val() != 4) {
+	 		comboSelect.removeClass('hidden');
+	 		$('#date').addClass('hidden');
+
+	 		if (obj.val() != "" || obj.val() == 4) {
+	 			$.post('control/ajax.front.php', {frontAjax: '',method: 'selectHistorial' , option: obj.val()}, function(data) {
+	 				/**
+	 				 * decoding
+	 				 */
+	 				var collection = $.parseJSON(data);
+	 				
+	 				comboSelect.empty();
+	 				
+	 				$.each(collection, function(index, val) {
+	 					comboSelect.append('<option value="'+val.value+'">'+val.text+'</option>');
+	 				});
+	 				$(window).trigger('loadAjax');
+	 				
+		 		});
+	 		}else{
+	 			comboSelect.empty();
+	 		}
+	 	}else{
+	 		comboSelect.addClass('hidden');
+	 		$('#date').removeClass('hidden');
+	 	}
+
+	 });
+
+
+	/**
+	 * Select persistente
+	 */
+	var select_option = $('#currentOption').val();
+	var select_option_val = parseInt($('#currentValOption').val());
+
+	var options = ['','producto','estado','remito','date'];
+	if (select_option != "") {
+		$.each(options, function(index, val) {
+			if (select_option == val) {
+				$('#findBy').val(index);
+			};
+		});
+		$('#findBy').trigger('change');
+
+		
+		console.info('Reporting try:', select_option_val);
+	};
+
+	$(window).on('loadAjax',function(){
+
+		$('#results').val(select_option_val);
+		// console.log($('#results'));
+	});
+
+
+
+
+
+	    
+});
+
+
---
+++ assets/js/form.validator.js
@@ -0,0 +1,117 @@
+(function($){
+
+	$.fn.message = function(param){
+		var messages = [
+			'respetar cantidad máxima',  // 0
+			'excedidó límite de canjes para este producto', 	// 1
+			'respetar cantidad minima', 	// 2
+			'Por favor, llene los campos de productos'  // 3
+		];
+		var options = $.extend(true, {}, param);
+		
+
+		if (!param.def) {
+			$(this).children('.text').text(messages[param.message]);
+			$(this).removeClass('hidden');
+		}else{
+			$(this).children('.text').text('');
+			$(this).addClass('hidden');
+		}
+
+	}
+
+})(jQuery);
+
+
+jQuery(document).ready(function($) {
+	/**
+	 * @mensajes
+	 */
+	
+
+
+	/**
+	 * FORMULARIO DE PRODUCTOS
+	 */
+	
+	var send = false;
+
+	var max = $('#max').val();
+	if (max == "notlimit") {
+		max = 9999999999;
+	};
+	var min = parseInt($('#min').val()) || 0;
+	var sended = 0;
+
+	$('#add_product').on('submit', function(event) {
+		event.preventDefault();
+		var total = 0;
+		
+
+
+		/**
+		 * Evaluo el total
+		 */
+		$('.input_talle').each(function(index, val) {
+			total += parseInt($(val).val()) || 0;
+		});
+
+		if (total < min) {
+			$('#error_1').message({message: 2});
+			event.preventDefault();
+		}else if(total > max){
+			$('#error_1').message({message: 0});
+			event.preventDefault();
+		}else if(total == 0){
+			$('#error_1').message({message: 3});
+			event.preventDefault();		
+		}else{
+			
+			$('.block-botones .boton').attr('disabled', '');
+			$('#error_1').message({def: true});
+			$(this).unbind(event);
+			$(this).submit();
+		}
+
+	});
+
+
+	$('.input_talle').change(function(event) {
+		event.preventDefault();
+		/**
+		 * params
+		 */
+		var total = 0;
+
+		/**
+		 * Evaluo el total
+		 */
+		$('.input_talle').each(function(index, el) {
+			total += parseInt($(el).val()) || 0;		
+		});
+		
+		// console.info('Reporting :', total , min , max);
+
+		if (total < min) {
+			$('#error_1').message({message: 2});
+		}else if(total > max){
+			$('#error_1').message({message: 0});
+		}else if(total == 0){
+			$('#error_1').message({message: 3});		
+		}else{
+			$('#error_1').message({def: true});
+		}
+
+		/**
+		 * Sanitizo inputs menores a 0
+		 */
+		if (parseInt($(this).val()) < 0 ) {
+			$(this).val(0);
+			$(this).trigger('change');
+		};
+
+	});
+
+
+
+});
